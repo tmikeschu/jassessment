4de08af72c10a2ff213fbfb7f86c1bf239b2a83f
Mike Schutte <tmikeschutte@gmail.com>
2017-07-30 19:00:03 -0600

    Add js logic

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,6 +63,7 @@
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
+<<<<<<< HEAD
 /******/ ([
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
@@ -74,11 +75,59 @@ module.exports = __webpack_require__(2);
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
+=======
+/******/ ({
+
+/***/ 8:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+throw new Error("Cannot find module \"./functions\"");
+
+>>>>>>> Add js logic
 
 document.addEventListener("DOMContentLoaded", () => {
-  // have fun!
+  const textSubmitButton = document.querySelector(".text-submission button")
+  const text = document.querySelector(".text-submission textarea")
+
+  textSubmitButton.addEventListener("click", handleTextSubmit(text))
+  text.addEventListener("keyup", textEnter(text))
 })
 
+function textEnter (text) {
+  return event => {
+    if (event.keyCode === 13) {
+      handleTextSubmit(text)(event)
+      return false
+    }
+  }
+}
+
+function handleTextSubmit (text) {
+  return event => {
+    const wordCount = wordCountFor(text.value)
+    text.value = ""
+    const words = Object.keys(wordCount)
+    const presenter = document.querySelector(".word-count")
+    words.forEach(word => {
+      const para = document.createElement("p")
+      para.innerHTML = `${word}<span>${wordCount[word]} times</span>`
+      para.style.fontSize = `${wordCount[word]}em`
+      para.tabIndex = 0
+      presenter.append(para)
+    })
+  }
+}
+
+function wordCountFor (text) {
+  const nonAlpha = /[^a-z]/
+  return text.split(nonAlpha).filter(x => x)
+    .reduce((acc, el) => {
+      acc[el] = (acc[el] || 0) + 1
+      return acc
+    }, {})
+}
 
 
 /***/ }),
--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
+import  { checkEnter, wordCountFor } from "./functions"
+
 document.addEventListener("DOMContentLoaded", () => {
-  // have fun!
+  const textSubmitButton = document.querySelector(".text-submission button")
+  const text = document.querySelector(".text-submission textarea")
+
+  textSubmitButton.addEventListener("click", handleTextSubmit(text))
+  text.addEventListener("keyup", textEnter(text))
 })
 
+function textEnter (text) {
+  return event => {
+    if (event.keyCode === 13) {
+      handleTextSubmit(text)(event)
+      return false
+    }
+  }
+}
+
+function handleTextSubmit (text) {
+  return event => {
+    const wordCount = wordCountFor(text.value)
+    text.value = ""
+    const words = Object.keys(wordCount)
+    const presenter = document.querySelector(".word-count")
+    words.forEach(word => {
+      const para = document.createElement("p")
+      para.innerHTML = `${word}<span>${wordCount[word]} times</span>`
+      para.style.fontSize = `${wordCount[word]}em`
+      para.tabIndex = 0
+      presenter.append(para)
+    })
+  }
+}
+
+function wordCountFor (text) {
+  const nonAlpha = /[^a-z]/
+  return text.split(nonAlpha).filter(x => x)
+    .reduce((acc, el) => {
+      acc[el] = (acc[el] || 0) + 1
+      return acc
+    }, {})
+}
